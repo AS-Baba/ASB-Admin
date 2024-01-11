5c7ebc77607f9636216d1ab38cf7ed5e3ed9a988
Abubakar Salihu <salihuabubakar557@gmail.com>
2024-01-11 08:58:38 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./Components/Home";
+import Error404 from "./Components/Error404";
+import Error500 from "./Components/Error500";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
+import { BrowserRouter } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Routes,
+} from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/error500" element={<Error500 />} />
+        <Route path="/error404" element={<Error404 />} />
+        {/* <Route path="*" element={<Error404/>}/> */}
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
